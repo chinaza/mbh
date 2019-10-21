e7036d26cc00dbdca19010983003da44c9e1ba44
Chinaza Egbo <chinaza@zhaptek.com>
2019-10-21 22:36:21 +0100

    refactor: add geocoding

--- app/controllers/gigs-twitter.js
+++ app/controllers/gigs-twitter.js
@@ -5,15 +5,21 @@ const requestCountry = require('request-country');
 module.exports = {
   async queryGigs(req, res) {
     try {
-      let { q = '' } = req.query;
+      let { q = '', pos } = req.query;
       const country = requestCountry(req);
 
       // Build job seaarch query
       q = `(${q} ${country ||
         ''}) ((ongoing OR URGENT OR job OR role) (recruitment OR vacancy OR hiring))`;
 
+      let opts = {};
+      if (pos) {
+        opts = { geocode: `${pos.latitude},${pos.longitude},1000km` };
+      }
+
       const tweets = await twitter.searchTweets({
-        q
+        q,
+        opts
       });
 
       return res.send({
