06bf142d28922963d166948b1d4de37dd8266185
Chinaza Egbo <chinaza@zhaptek.com>
2019-10-21 17:22:01 +0100

    refactor: add security features

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 require('./config');
 
-const indexRouter = require('./routes/index');
+const router = require('./routes/index');
 
 const app = express();
 
@@ -18,9 +20,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(helmet());
+app.use(cors());
+
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
