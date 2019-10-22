4f23f351a5d2e8a8287e3a5ef14e0d784e9dbea1
Chinaza Egbo <chinaza@zhaptek.com>
2019-10-22 10:27:44 +0100

    refactor: filter out brexit

--- app/controllers/gigs-twitter.js
+++ app/controllers/gigs-twitter.js
@@ -12,7 +12,7 @@ module.exports = {
 
       // Build job seaarch query
       q = `(${q} ${country ||
-        ''}) ((ongoing OR URGENT OR job OR role) (recruitment OR vacancy OR hiring))`;
+        ''}) ((ongoing OR URGENT OR job OR role) (recruitment OR vacancy OR hiring)) -brexit`;
 
       let opts = {};
       if (pos) {
